4408dfb7fd3058327746264b5225bf5d03c9d861
Bryan P Johnson <bryan@12ftguru.com>
2017-10-25 14:08:28 -0400

    Pass lint.

--- pages/index.spec.js
+++ pages/index.spec.js
@@ -2,15 +2,16 @@ import Vue from 'vue';
 import App from '~/pages/index';
 
 describe('index.spec.js', () => {
-  let cmp, vm;
+  let AppTest, vm;
 
   beforeEach(() => {
-    cmp = Vue.extend(App); // Create a copy of the original component
-    vm = new cmp({}).$mount(); // Instances and mounts the component
+    AppTest = Vue.extend(App); // Create a copy of the original component
+    vm = new AppTest({}).$mount(); // Instances and mounts the component
   });
 
   it('has an h1 with a title of "toolkit"', () => {
-    expect(vm.$el.querySelector('h1.title').textContent)
-      .toMatch(/^\s*toolkit\s*$/);
+    expect(vm.$el.querySelector('h1.title').textContent).toMatch(
+      /^\s*toolkit\s*$/
+    );
   });
 });
