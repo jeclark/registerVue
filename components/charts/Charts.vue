d4ec5706b14e27ccc1e0fd13c2c2a4a745cac516
John Clark <john@12ftguru.com>
2018-02-07 13:44:24 -0500

    Charts working

--- components/charts/Charts.vue
+++ components/charts/Charts.vue
@@ -1,16 +1,62 @@
 <template>
-  <div class="charts">
-    Chart Stuff Here
-  </div>
+<div class="charts">
+  <highcharts :options="options" ref="highcharts"></highcharts>
+</div>
 </template>
 
 <script>
-// Nothing to see here.
+import Vue from 'vue';
+import VueHighcharts from 'vue-highcharts';
+Vue.use(VueHighcharts);
+export default {
+  data() {
+    return {
+      options: {
+        chart: {
+          type: 'bar'
+        },
+        title: {
+          text: 'Stacked bar chart'
+        },
+        xAxis: {
+          categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Total fruit consumption'
+          }
+        },
+        legend: {
+          reversed: true
+        },
+        plotOptions: {
+          series: {
+            stacking: 'normal'
+          }
+        },
+        series: [
+          {
+            name: 'John',
+            data: [5, 3, 4, 7, 2]
+          },
+          {
+            name: 'Jane',
+            data: [2, 2, 3, 2, 1]
+          },
+          {
+            name: 'Joe',
+            data: [3, 4, 4, 2, 5]
+          }
+        ]
+      }
+    };
+  }
+};
 </script>
 
 <style>
 .charts {
-  background-color: lightblue;
-  padding: 10px;
+  margin-right: 5px;
 }
 </style>
