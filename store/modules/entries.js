370f5454fbd6a6e318bd667ff121e7d46561473f
John Clark <john@12ftguru.com>
2018-02-28 11:44:20 -0500

    Still borked

--- store/modules/entries.js
+++ store/modules/entries.js
@@ -8,31 +8,33 @@ const state = {
 const getters = {
   return_sorted_entries: state => {
     // this really should be a date sort
-    console.log('return_sorted_entries state is ', state);
+    // console.log('return_sorted_entries state is ', state.entries);
     if (state.entries !== undefined) {
       var sortedEntries = state.entries.slice(
         Math.max(state.entries.length - 100, 1)
       );
       console.log('sorted entries are ', sortedEntries.reverse());
       return sortedEntries.reverse();
+    } else {
+      return [];
     }
   },
   return_total: state => {
-    console.log('return_total state is ', state);
+    // console.log('return_total state is ', state);
     if (state.entries !== undefined) {
-      console.log('Got entries!');
+      // console.log('Got entries!');
       var total = parseFloat(state.entries[0].amount);
       state.entries.reduce(function(entries, entry) {
         total = total + parseFloat(entry.amount);
       });
       return total.toFixed(2);
     } else {
-      console.log('No entries! BOOOOOOO!');
+      // console.log('No entries! BOOOOOOO!');
       return 0;
     }
   },
   return_cleared_total: state => {
-    console.log('return_cleared_total state is ', state);
+    // console.log('return_cleared_total state is ', state);
     if (state.entries !== undefined) {
       var total = parseFloat(state.entries[0].amount);
       state.entries.reduce(function(entries, entry) {
@@ -52,7 +54,7 @@ const actions = {
   getAllEntries({ commit }) {
     // console.log('called getAllEntries');
     axios.get('http://simple-rest-api.dev/api/entry/').then(resp => {
-      console.log('getAllEntries and data is ', resp.data);
+      // console.log('getAllEntries and data is ', resp.data);
       commit('SET_ENTRIES', resp.data); // using response.data
     });
   }
@@ -63,7 +65,7 @@ const mutations = {
   SET_ENTRIES(state, data) {
     console.log('called SET_ENTRIES with ', data.data);
     state.entries = data.data;
-    console.log('store is set to ', state);
+    // console.log('store is set to ', state);
   }
 };
 
