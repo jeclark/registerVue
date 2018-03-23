4007000dbb0748a24a93730c24d6e34601736e8b
John Clark <john@12ftguru.com>
2018-03-23 11:39:46 -0400

    charts both working

--- store/modules/charts.js
+++ store/modules/charts.js
@@ -1,14 +1,24 @@
 import axios from 'axios';
 import moment from 'moment';
 import https from 'https';
+import lodash from 'lodash';
 
 export const state = () => ({
   lineData: [],
-  last12months: []
+  last12months: [],
+  catList: [
+    'groceries',
+    'utilities',
+    'entertainment',
+    'gas',
+    'woodworking',
+    'restaurant'
+  ]
 });
 // getters
 const getters = {
   get_lines(state, getters) {
+    console.log('called get_lines with months', state.last12months);
     console.log('called get_lines with state', state.lineData);
     var checkTag = '';
     var outputArray = [];
@@ -17,33 +27,30 @@ const getters = {
       .startOf('month')
       .subtract(12, 'months')
       .format('YYYY-MM');
-    for (var key in state.lineData) {
-      // console.log('line is ', data.data.data[key]);
-      // this is where we do our really dumb loop because highcharts data is silly
-      // set the $checkTag to the tag of the first row
-      if (state.lineData[key].tag !== checkTag) {
-        // we have a new check tag
-        // do we have data?
-        if (tempData.length !== 0) {
-          outputArray.push({
-            name: checkTag,
-            data: tempData
-          });
+    // loop through catList
+    state.catList.forEach(function(category) {
+      tempData[category] = [];
+      console.log('tempData is ', tempData);
+      // loop through the last 12 months
+      state.last12months.forEach(function(eachMonth) {
+        var data = lodash.find(state.lineData, function(obj) {
+          return obj.yearMonth === eachMonth && obj.tag === category;
+        });
+        // if we have a value for the month, we set it, otherwise it is set to 0
+        if (typeof data !== 'undefined') {
+          tempData[category].push(Math.abs(data.total));
+        } else {
+          tempData[category].push(0);
         }
-        tempData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        // console.log('new checkTag', data.data.data[key].tag);
-        checkTag = state.lineData[key].tag;
-      }
-      if (state.lineData[key].yearMonth > range) {
-        // these are the ones we care about
-        tempData[state.lineData[key].month - 1] = Math.abs(
-          state.lineData[key].total
-        );
-        // console.log('tempData is ', tempData);
-      } else {
-        // don't care about these
-        // console.log('date is ', data.data.data[key].yearMonth);
-      }
+      });
+      console.log('for category this is tempData', category, tempData);
+    });
+    // at the end of the loop we add things to our outputArray
+    for (var key in tempData) {
+      outputArray.push({
+        name: key,
+        data: tempData[key]
+      });
     }
     // console.log('outputArray is ', outputArray);
     console.log('called get_lines returned ', outputArray);
